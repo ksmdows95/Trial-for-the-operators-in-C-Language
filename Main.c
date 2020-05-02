76fc91476a8a43d2075893896dc769ff7e5bf557
Kamer Said MOR <43989742+ksmdows95@users.noreply.github.com>
2020-05-02 17:14:04 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -5,6 +5,7 @@ int main()
 {
 	int a = 10;
 	int b = 20;
+	//I tried to see that a+++b operator works like a++ + b or a + ++b
 	int c = a+++b;
 	printf("a= %d , b= %d , c= %d \n", a, b, c);
 	return 0;
